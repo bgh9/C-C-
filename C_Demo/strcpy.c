f4b478c55e766efffd85344c15261d83048b8143
weimq <1570096859@qq.com>
2020-09-24 21:17:04 +0800

    添加拷贝字符串文件（strcpy.c）

---
+++ C_Demo/strcpy.c
@@ -0,0 +1,31 @@
+//功能：字符串拷贝函数的实现
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+//拷贝函数
+char *strcpy(char* dest, const char* src) //源字符串使用const表示输入参数
+{
+    //对目的地址和源地址进行非空断言处理
+    assert( (dest != NULL) && (src != NULL) );
+
+    char *temp = dest; 
+    while( (*dest++ = *src++) != '\0'); //根据'\0'判断是否拷贝结束
+    return temp; //为了实现链式操作，返回地址
+}
+
+int main()
+{
+    printf("*** strcpy test ***\r\n");
+
+    char str[10];
+    if( strcpy(str,"1234") != NULL )
+    {
+        printf("str:%s\r\n",str);
+        printf("copy success!\r\n");
+    }
+
+    printf("\r\n");
+    system("pause");
+    return 0;
+}
